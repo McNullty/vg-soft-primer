4424d9d0402e4da26fa34e6b58299f80c85eee95
Mladen Čikara <mladen.cikara@gmail.com>
2019-10-05 00:24:54 +0200

    Initil import.

---
+++ src/main/java/hr/vgsoft/primer/PrimerApplication.java
@@ -0,0 +1,13 @@
+package hr.vgsoft.primer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PrimerApplication.class, args);
+  }
+
+}
