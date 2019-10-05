74eb8f2cd97ebaf895e7f448a6bf0f0ae9ace2fe
Mladen Čikara <mladen.cikara@gmail.com>
2019-10-05 15:22:50 +0200

    Creating new backend module

---
+++ backend/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    java
+    id("org.springframework.boot") version "2.1.9.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    id("org.asciidoctor.convert") version "1.5.8"
+}
+
+group = "hr.vgsoft"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+val snippetsDir = file("build/generated-snippets")
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation ("org.springframework.boot:spring-boot-starter-web")
+    compileOnly ("org.projectlombok:lombok")
+    annotationProcessor ("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor ("org.projectlombok:lombok")
+    testImplementation ("org.springframework.boot:spring-boot-starter-test")
+    testImplementation ("org.springframework.restdocs:spring-restdocs-mockmvc")
+}
+
+tasks.test {
+    outputs.dir(snippetsDir)
+}
+
+tasks.asciidoctor {
+    inputs.dir(snippetsDir)
+    dependsOn(tasks.test)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "primer"
+
+include("backend")
