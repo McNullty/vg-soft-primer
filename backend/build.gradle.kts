874a75db1e9edfca768f26b98a6f949fb94f9a9d
Mladen Čikara <mladen.cikara@gmail.com>
2019-10-06 11:23:17 +0200

    Moving Jacoco configuration in separate file

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
     java
     idea
-    jacoco
     id("org.springframework.boot") version "2.1.9.RELEASE"
     id("io.spring.dependency-management") version "1.0.8.RELEASE"
     id("org.asciidoctor.convert") version "1.5.8"
 }
 
 apply(from = "../gradle/integrationTest.gradle.kts")
+apply(from = "../gradle/jacoco.gradle.kts")
 
 group = "hr.vgsoft"
 
@@ -55,66 +55,3 @@ tasks.asciidoctor {
     dependsOn(tasks.test)
 }
 
-tasks.jacocoTestReport {
-    reports {
-        csv.isEnabled = false
-        html.isEnabled = true
-        xml.isEnabled = false
-
-        html.destination = file("${buildDir}/reports/jacocoAllTestHtml")
-    }
-
-    executionData(tasks["test"], tasks["integrationTest"])
-    dependsOn(tasks["test"], tasks["integrationTest"])
-}
-
-task<JacocoReport>("jacocoIntegrationTestReport") {
-    description = "Generates code coverage report for integrationTest task"
-    group = "verification"
-
-    reports {
-        csv.isEnabled = false
-        html.isEnabled = true
-        xml.isEnabled = false
-
-        html.destination = file("${buildDir}/reports/jacocoIntegrationTestHtml")
-    }
-
-    executionData(tasks["integrationTest"])
-    sourceSets(sourceSets.getByName("main"))
-    dependsOn(tasks["integrationTest"])
-}
-
-task<JacocoReport>("jacocoUnitTestReport") {
-    description = "Generates code coverage report only for test task"
-    group = "verification"
-
-    reports {
-        csv.isEnabled = false
-        html.isEnabled = true
-        xml.isEnabled = false
-
-        html.destination = file("${buildDir}/reports/jacocoUnitTestHtml")
-    }
-
-    executionData(tasks["test"])
-    sourceSets(sourceSets.getByName("main"))
-    dependsOn(tasks["test"])
-}
-
-
-
-tasks.jacocoTestCoverageVerification {
-    violationRules {
-        rule {
-            limit {
-                minimum = "0.8".toBigDecimal()
-            }
-        }
-    }
-    executionData(tasks["test"], tasks["integrationTest"])
-    sourceSets(sourceSets.getByName("main"))
-    dependsOn(tasks["test"], tasks["integrationTest"])
-}
-
-tasks.named("check") { dependsOn(tasks["jacocoTestCoverageVerification"]) }
---
+++ gradle/jacoco.gradle.kts
@@ -0,0 +1,66 @@
+apply( plugin = "jacoco")
+
+val sourceSets = the<SourceSetContainer>()
+
+
+tasks.named<JacocoReport>("jacocoTestReport") {
+    reports {
+        csv.isEnabled = false
+        html.isEnabled = true
+        xml.isEnabled = false
+
+        html.destination = file("${buildDir}/reports/jacocoAllTestHtml")
+    }
+
+    executionData(tasks["test"], tasks["integrationTest"])
+    dependsOn(tasks["test"], tasks["integrationTest"])
+}
+
+task<JacocoReport>("jacocoIntegrationTestReport") {
+    description = "Generates code coverage report for integrationTest task"
+    group = "verification"
+
+    reports {
+        csv.isEnabled = false
+        html.isEnabled = true
+        xml.isEnabled = false
+
+        html.destination = file("${buildDir}/reports/jacocoIntegrationTestHtml")
+    }
+
+    executionData(tasks["integrationTest"])
+    sourceSets(sourceSets.getByName("main"))
+    dependsOn(tasks["integrationTest"])
+}
+
+task<JacocoReport>("jacocoUnitTestReport") {
+    description = "Generates code coverage report only for test task"
+    group = "verification"
+
+    reports {
+        csv.isEnabled = false
+        html.isEnabled = true
+        xml.isEnabled = false
+
+        html.destination = file("${buildDir}/reports/jacocoUnitTestHtml")
+    }
+
+    executionData(tasks["test"])
+    sourceSets(sourceSets.getByName("main"))
+    dependsOn(tasks["test"])
+}
+
+tasks.named<JacocoCoverageVerification>("jacocoTestCoverageVerification") {
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.8".toBigDecimal()
+            }
+        }
+    }
+    executionData(tasks["test"], tasks["integrationTest"])
+    sourceSets(sourceSets.getByName("main"))
+    dependsOn(tasks["test"], tasks["integrationTest"])
+}
+
+tasks.named("check") { dependsOn(tasks["jacocoTestCoverageVerification"]) }
