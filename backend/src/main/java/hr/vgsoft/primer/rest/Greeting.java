c732765d5c217d92fc03bb94c599944b8c06190c
Mladen Čikara <mladen.cikara@gmail.com>
2019-10-13 15:07:39 +0200

    REST controller

---
+++ backend/src/main/java/hr/vgsoft/primer/rest/Greeting.java
@@ -0,0 +1,20 @@
+package hr.vgsoft.primer.rest;
+
+public class Greeting {
+
+  private final long id;
+  private final String content;
+
+  Greeting(long id, String content) {
+    this.id = id;
+    this.content = content;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
