38052bf8ec573b143f9d9917c596139028bd8bb9
Mladen Čikara <mladen.cikara@gmail.com>
2019-10-15 21:27:35 +0200

    Creating task that copies index html to right place

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -8,6 +8,11 @@ tasks.register<Exec>("elmMake") {
     commandLine = listOf("elm", "make", "$projectDir/src/Main.elm", "--output", jsPath )
 }
 
+tasks.register<Copy>("copyIndexHtml") {
+    from("src/index.html")
+    into("$projectDir/build/")
+}
+
 tasks.register<Delete>("clean") {
     delete.add("elm/elm-stuff/0.19.0")
     delete.add("elm/elm-stuff/generated-code")
@@ -18,13 +23,16 @@ tasks.register<Delete>("clean") {
 
 tasks.register<Copy>("copyElmApp") {
     val elmMake by tasks.getting
+    val copyIndexHtml by tasks.getting
 
     val processResources by project(":backend").tasks.existing(ProcessResources::class)
 
     from(elmMake.outputs)
+    from(copyIndexHtml.outputs)
     into(processResources.get().destinationDir.toString() + "/public")
 
     dependsOn(":frontend:elmMake")
+    dependsOn(":frontend:copyIndexHtml")
 }
 
 project(":backend") {
