1bf4cf2add9354cb17b53eff3d4b0f9da17e827a
Mladen Čikara <mladen.cikara@gmail.com>
2020-01-14 20:16:28 +0100

    Moving flyway scripts to different directory

---
+++ backend/src/main/resources/db/migration/dev/V1.0.0.003__Insert_test_data_to_Items_table.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO ITEMS(uuid, name, description) VALUES ('0840f804-3701-11ea-aec2-2e728ce88125', 'Item1', 'First item');
+INSERT INTO ITEMS(uuid, name, description) VALUES ('0840fa7a-3701-11ea-aec2-2e728ce88125', 'Item2', 'Second item');
+INSERT INTO ITEMS(uuid, name, description) VALUES ('0840fbe2-3701-11ea-aec2-2e728ce88125', 'Item3', 'Third item');
