fd4b8bb66a718500b5d6d1f148a74862028413d2
Mladen Čikara <mladen.cikara@gmail.com>
2020-01-17 17:55:57 +0100

    Adding self link for item

--- backend/src/main/java/hr/vgsoft/primer/item/ItemModel.java
+++ backend/src/main/java/hr/vgsoft/primer/item/ItemModel.java
@@ -3,7 +3,9 @@ package hr.vgsoft.primer.item;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 
 @EqualsAndHashCode(callSuper = true)
 @Value
@@ -14,5 +16,10 @@ public class ItemModel extends RepresentationModel<ItemModel> {
   public ItemModel(final Item item) {
     this.name = item.getName();
     this.description = item.getDescription();
+
+    final Link selfRelLink = WebMvcLinkBuilder
+            .linkTo(WebMvcLinkBuilder.methodOn(ItemController.class).getItem(item.getUuid()))
+            .withSelfRel();
+    add(selfRelLink);
   }
 }
