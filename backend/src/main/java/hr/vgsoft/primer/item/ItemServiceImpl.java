ae47fecb350bdcee570c6afa6f3daaedcf6a76cd
Mladen Čikara <mladen.cikara@gmail.com>
2020-03-10 15:11:47 +0100

    Configure caching on Items service

--- backend/src/main/java/hr/vgsoft/primer/item/ItemServiceImpl.java
+++ backend/src/main/java/hr/vgsoft/primer/item/ItemServiceImpl.java
@@ -2,6 +2,10 @@ package hr.vgsoft.primer.item;
 
 import java.util.UUID;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -17,18 +21,22 @@ public class ItemServiceImpl implements ItemService {
     this.itemRepository = itemRepository;
   }
 
+  @Cacheable("items")
   @Transactional(readOnly = true)
   @Override
   public Page<Item> findAll(final Pageable pageable) {
     return itemRepository.findAll(pageable);
   }
 
+  @Cacheable("item")
   @Transactional(readOnly = true)
   @Override
   public Item getItemByUuid(final UUID uuid) {
     return itemRepository.findById(uuid).orElseThrow(() -> new ItemNotFoundException(uuid));
   }
 
+
+  @CachePut(value = "item", key = "#result.uuid")
   @Override
   public Item newItem(final NewItemModel itemModel) {
 
@@ -38,6 +46,10 @@ public class ItemServiceImpl implements ItemService {
   }
 
 
+  @Caching(evict = {
+          @CacheEvict(value = "item", key = "#uuid"),
+          @CacheEvict(value = "items", allEntries = true),
+  })
   @Override
   public void updateItem(final UUID uuid, final NewItemModel updateItem) {
 
@@ -48,6 +60,10 @@ public class ItemServiceImpl implements ItemService {
     item.setDescription(updateItem.getDescription());
   }
 
+  @Caching(evict = {
+          @CacheEvict(value = "item", key = "#uuid"),
+          @CacheEvict(value = "items", allEntries = true),
+  })
   @Override
   public void deleteItem(final UUID uuid) {
     final Item item =
