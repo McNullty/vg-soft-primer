1563b950f702f7b136770132318a09053f28a4ab
Mladen Čikara <mladen.cikara@gmail.com>
2020-03-14 12:26:10 +0100

    Refactor: function for processing metadata and body is passed in

--- frontend/src/ItemsHttpClient.elm
+++ frontend/src/ItemsHttpClient.elm
@@ -10,7 +10,7 @@ import RemoteData exposing (WebData)
 
 
 type alias ItemsResponseBody =
-    { items : (List Item)
+    { items : List Item
     , page : PagingData
     }
 
@@ -45,8 +45,8 @@ pageDecoder =
 itemsResponseDecoder : Decode.Decoder ItemsResponseBody
 itemsResponseDecoder =
     Decode.succeed ItemsResponseBody
-            |> Pipeline.optionalAt ["_embedded", "items"] (Decode.list itemDecoder) []
-            |> Pipeline.requiredAt ["page"] pageDecoder
+        |> Pipeline.optionalAt [ "_embedded", "items" ] (Decode.list itemDecoder) []
+        |> Pipeline.requiredAt [ "page" ] pageDecoder
 
 
 getEtagFromHeader : Dict String String -> Maybe String
@@ -55,21 +55,29 @@ getEtagFromHeader headers =
 
 
 {-|
+
     This function uses header metadata and body from response to create ItemsResponse type
+
 -}
 processMetadataAndBody : Metadata -> String -> Result Decode.Error ItemsResponse
 processMetadataAndBody metadata body =
     let
-        etagResult = getEtagFromHeader metadata.headers
+        etagResult =
+            getEtagFromHeader metadata.headers
 
-        bodyResult = Decode.decodeString itemsResponseDecoder body
-        response  =
+        bodyResult =
+            Decode.decodeString itemsResponseDecoder body
+
+        response =
             case bodyResult of
-                Result.Ok value ->  Result.Ok { body = value
-                                              , etag = etagResult
-                                              }
-                Result.Err err -> Result.Err err
+                Result.Ok value ->
+                    Result.Ok
+                        { body = value
+                        , etag = etagResult
+                        }
 
+                Result.Err err ->
+                    Result.Err err
     in
     response
 
@@ -77,6 +85,7 @@ processMetadataAndBody metadata body =
 {-| This function takes parsed result that was already put in type and creates appropriate application message
 
     This function will take results form parser and create application message depending on result
+
 -}
 customResultToMessage : (FetchingResults -> a) -> (Result Decode.Error ItemsApiResponses -> a)
 customResultToMessage converter result =
@@ -85,71 +94,113 @@ customResultToMessage converter result =
             case result of
                 Result.Ok value ->
                     case value of
-                        Success itemResponse -> ItemsReceived (RemoteData.Success itemResponse)
-                        NotModified -> ItemsNotModified
-                        CustomError errorMessage-> FetchError errorMessage
+                        Success itemResponse ->
+                            ItemsReceived (RemoteData.Success itemResponse)
+
+                        NotModified ->
+                            ItemsNotModified
+
+                        CustomError errorMessage ->
+                            FetchError errorMessage
+
                 Result.Err errorMessage ->
                     FetchError ("Custom error message" ++ Decode.errorToString errorMessage)
     in
     converter fetchingResults
 
 
-
 {-|
+
     This function gets response from HTTP client and translates it to type or error.
 
     Output fom this function will be passed to  customFromResult
+
 -}
-customResponseToResult : Response String -> Result Decode.Error ItemsApiResponses
-customResponseToResult response =
+customResponseToResult :
+    Response String
+    -> (Metadata -> String -> Result Decode.Error ItemsResponse)
+    -> Result Decode.Error ItemsApiResponses
+customResponseToResult response metadataAndBodyProcessor =
     let
-        _ = Debug.log "Response" response
+        _ =
+            Debug.log "Response" response
     in
     case response of
         BadUrl_ url ->
             Result.Ok (CustomError ("Bad url: " ++ url))
+
         Timeout_ ->
             Result.Ok (CustomError "Timeout")
+
         NetworkError_ ->
             Result.Ok (CustomError "Network error")
+
         BadStatus_ metadata body ->
             let
-                _ = Debug.log "Metadata status" metadata.statusCode
-                _ = Debug.log "Bad status body" body
+                _ =
+                    Debug.log "Metadata status" metadata.statusCode
+
+                _ =
+                    Debug.log "Bad status body" body
             in
             case metadata.statusCode of
-                304 -> Result.Ok NotModified
-                _ -> Result.Ok (CustomError ("Mad status" ++ String.fromInt metadata.statusCode ++ body))
+                304 ->
+                    Result.Ok NotModified
+
+                _ ->
+                    Result.Ok (CustomError ("Mad status" ++ String.fromInt metadata.statusCode ++ body))
+
         GoodStatus_ metadata body ->
             case metadata.statusCode of
-                200 -> let
-                           processingResponse = processMetadataAndBody metadata body
-                           apiResponse =
-                               case processingResponse of
-                                   Result.Ok value -> Result.Ok (Success value)
-                                   Result.Err error ->
-                                       Result.Ok (CustomError ("Error while parsing: " ++ Decode.errorToString error))
-                       in
-                       apiResponse
-                _ -> Result.Ok (CustomError
-                       ("Good status but with unknown value: " ++ (String.fromInt metadata.statusCode)))
+                200 ->
+                    let
+                        processingResponse =
+                            metadataAndBodyProcessor metadata body
+
+                        apiResponse =
+                            case processingResponse of
+                                Result.Ok value ->
+                                    Result.Ok (Success value)
+
+                                Result.Err error ->
+                                    Result.Ok (CustomError ("Error while parsing: " ++ Decode.errorToString error))
+                    in
+                    apiResponse
+
+                _ ->
+                    Result.Ok
+                        (CustomError
+                            ("Good status but with unknown value: " ++ String.fromInt metadata.statusCode)
+                        )
 
 
-customExpectFunction : (FetchingResults -> a) -> Http.Expect a
-customExpectFunction converter =
-        expectStringResponse (customResultToMessage converter) customResponseToResult
+customExpectFunction :
+    (FetchingResults -> a)
+    -> (Response String -> Result Decode.Error ItemsApiResponses)
+    -> Http.Expect a
+customExpectFunction converter responseToResult =
+    expectStringResponse (customResultToMessage converter) responseToResult
+
+
+responseToItemsApiResponse : Response String -> Result Decode.Error ItemsApiResponses
+responseToItemsApiResponse response =
+    customResponseToResult response processMetadataAndBody
 
 
 fetchItems : Int -> Maybe String -> (FetchingResults -> a) -> Cmd a
 fetchItems pageNumber etag convertToMsg =
     Http.request
         { method = "GET"
-        , headers = case etag of
-            Just tag -> [Http.header "If-None-Match" tag]
-            Nothing -> []
+        , headers =
+            case etag of
+                Just tag ->
+                    [ Http.header "If-None-Match" tag ]
+
+                Nothing ->
+                    []
         , url = "/api/items?page=" ++ String.fromInt pageNumber
         , body = Http.emptyBody
-        , expect = customExpectFunction convertToMsg
+        , expect = customExpectFunction convertToMsg responseToItemsApiResponse
         , timeout = Nothing
         , tracker = Nothing
         }
--- frontend/tests/ItemsHttpClientTest.elm
+++ frontend/tests/ItemsHttpClientTest.elm
@@ -10,6 +10,7 @@ import Pages.Items.ListItems as ListItems exposing (Msg(..), convertToMsg)
 import RemoteData exposing (WebData)
 import Test exposing (Test, describe, test)
 
+
 suite : Test
 suite =
     describe "Test suit for ItemsHttpClient"
@@ -17,67 +18,87 @@ suite =
             [ test "when body is returned" <|
                 \_ ->
                     let
-                        body = createBody
+                        body =
+                            createBody
 
-                        result = Decode.decodeString ItemsHttpClient.itemsResponseDecoder body
+                        result =
+                            Decode.decodeString ItemsHttpClient.itemsResponseDecoder body
 
-                        expected = createExpectedResultBody
+                        expected =
+                            createExpectedResultBody
                     in
-                        Expect.equal expected (result)
+                    Expect.equal expected result
             ]
         , describe "Testing body and metadata parser"
             [ test "when body and metadata are returned with status 200" <|
                 \_ ->
                     let
-                        metadata = createMetadata
-                        body = createBody
+                        metadata =
+                            createMetadata
 
-                        result = ItemsHttpClient.processMetadataAndBody metadata body
+                        body =
+                            createBody
 
-                        expected = createExpectedResult
+                        result =
+                            ItemsHttpClient.processMetadataAndBody metadata body
+
+                        expected =
+                            createExpectedResult
                     in
-                        Expect.equal expected (result)
+                    Expect.equal expected result
             ]
         , describe "Testing customFromResult function"
             [ test "when body and metadata are returned with status 200" <|
                 \_ ->
                     let
-                        metadata = createMetadata
-                        body = createBody
+                        metadata =
+                            createMetadata
 
+                        body =
+                            createBody
 
                         result =
                             GoodStatus_ metadata body
-                                |> ItemsHttpClient.customResponseToResult
+                                |> ItemsHttpClient.responseToItemsApiResponse
                                 |> ItemsHttpClient.customResultToMessage convertToMsg
 
-                        expected = createExpectedMsg
+                        expected =
+                            createExpectedMsg
                     in
-                        Expect.equal expected (result)
+                    Expect.equal expected result
             ]
         ]
 
+
+
 ---- Util methods
+
 
 createMetadata : Metadata
 createMetadata =
     { url = "http://localhost:8080/api/items?page=0"
     , statusCode = 200
     , statusText = ""
-    , headers = Dict.fromList
-        [ ("cache-control","max-age=2592000")
-        , ("connection","keep-alive")
-        , ("content-type","application/hal+json")
-        , ("date","Thu, 12 Mar 2020 10:09:35 GMT")
-        , ("etag","\"dc8b09bc90ed194d61b3111aa371e9dc\"")
-        , ("keep-alive","timeout=60")
-        , ("transfer-encoding","chunked")] }
+    , headers =
+        Dict.fromList
+            [ ( "cache-control", "max-age=2592000" )
+            , ( "connection", "keep-alive" )
+            , ( "content-type", "application/hal+json" )
+            , ( "date", "Thu, 12 Mar 2020 10:09:35 GMT" )
+            , ( "etag", "\"dc8b09bc90ed194d61b3111aa371e9dc\"" )
+            , ( "keep-alive", "timeout=60" )
+            , ( "transfer-encoding", "chunked" )
+            ]
+    }
+
 
 createExpectedResultBody : Result Decode.Error ItemsHttpClient.ItemsResponseBody
 createExpectedResultBody =
-    Result.Ok { items = [{ description = "Description for first item", id = ItemId "ddc56cf6-1736-4f8d-bfb0-8330cd3d3654", name = "TestItem1" },{ description = "Description for second item", id = ItemId "dacf77b6-5e70-439a-8e3b-d2da974d73ff", name = "TestItem2" },{ description = "Description for 3. item", id = ItemId "21fb97c7-53d4-4395-bc1e-d3e085f396a2", name = "TestItem3" },{ description = "Description for 4. item", id = ItemId "8146f000-ff27-40ed-865c-f58889a0f997", name = "TestItem4" },{ description = "Description for 5. item", id = ItemId "107d3693-0733-409c-9617-f50798cb4c4e", name = "TestItem5" },{ description = "Description for 6. item", id = ItemId "80e85bb1-0647-4445-b586-01900c0d5b34", name = "TestItem6" },{ description = "Description for 7. item", id = ItemId "6406e01b-d5c8-4c40-b551-cf49b3379599", name = "TestItem7" },{ description = "Description for 8. item", id = ItemId "b8c05299-2f0e-4acb-b6d3-27494fb4f404", name = "TestItem8" },{ description = "Description for 9. item", id = ItemId "54f3cc02-a7fd-4ca7-b732-e1f3e27c3984", name = "TestItem9" },{ description = "Description for 10. item", id = ItemId "4b66ed66-4610-4454-af38-a15bdd5c5844", name = "TestItem10" },{ description = "Description for 11. item", id = ItemId "461cfa07-f2c4-4d1e-ada9-5c592b461d17", name = "TestItem11" },{ description = "Description for 12. item", id = ItemId "00fadde0-8b5f-49d3-b6fd-d0f44cf065ed", name = "TestItem12" },{ description = "Description for 13. item", id = ItemId "de718ba8-520c-4199-a819-1674e5582bbe", name = "TestItem13" },{ description = "Description for 14. item", id = ItemId "b1e701fe-7188-47d2-8654-7808ced2f6a7", name = "TestItem14" },{ description = "Description for 15. item", id = ItemId "86aa8336-6bd0-440a-97fe-b51e1b9f6f9e", name = "TestItem15" },{ description = "Description for 16. item", id = ItemId "6d952799-5601-432a-9947-adad57bb54db", name = "TestItem16" },{ description = "Description for 17. item", id = ItemId "365aefff-1fb6-4b3e-84a9-18b1a256b76b", name = "TestItem17" },{ description = "Description for 18. item", id = ItemId "0353f12a-6100-49c4-9c13-262c6efe5019", name = "TestItem18" },{ description = "Description for 19. item", id = ItemId "00a9d79c-8642-4f3c-992c-5feff24da334", name = "TestItem19" },{ description = "Description for 20. item", id = ItemId "76cdfb25-3fd2-4f48-b16e-539968d22bba", name = "TestItem20" }]
-              , page = { number = 0, size = 20, totalElements = 50, totalPages = 3 }
-              }
+    Result.Ok
+        { items = [ { description = "Description for first item", id = ItemId "ddc56cf6-1736-4f8d-bfb0-8330cd3d3654", name = "TestItem1" }, { description = "Description for second item", id = ItemId "dacf77b6-5e70-439a-8e3b-d2da974d73ff", name = "TestItem2" }, { description = "Description for 3. item", id = ItemId "21fb97c7-53d4-4395-bc1e-d3e085f396a2", name = "TestItem3" }, { description = "Description for 4. item", id = ItemId "8146f000-ff27-40ed-865c-f58889a0f997", name = "TestItem4" }, { description = "Description for 5. item", id = ItemId "107d3693-0733-409c-9617-f50798cb4c4e", name = "TestItem5" }, { description = "Description for 6. item", id = ItemId "80e85bb1-0647-4445-b586-01900c0d5b34", name = "TestItem6" }, { description = "Description for 7. item", id = ItemId "6406e01b-d5c8-4c40-b551-cf49b3379599", name = "TestItem7" }, { description = "Description for 8. item", id = ItemId "b8c05299-2f0e-4acb-b6d3-27494fb4f404", name = "TestItem8" }, { description = "Description for 9. item", id = ItemId "54f3cc02-a7fd-4ca7-b732-e1f3e27c3984", name = "TestItem9" }, { description = "Description for 10. item", id = ItemId "4b66ed66-4610-4454-af38-a15bdd5c5844", name = "TestItem10" }, { description = "Description for 11. item", id = ItemId "461cfa07-f2c4-4d1e-ada9-5c592b461d17", name = "TestItem11" }, { description = "Description for 12. item", id = ItemId "00fadde0-8b5f-49d3-b6fd-d0f44cf065ed", name = "TestItem12" }, { description = "Description for 13. item", id = ItemId "de718ba8-520c-4199-a819-1674e5582bbe", name = "TestItem13" }, { description = "Description for 14. item", id = ItemId "b1e701fe-7188-47d2-8654-7808ced2f6a7", name = "TestItem14" }, { description = "Description for 15. item", id = ItemId "86aa8336-6bd0-440a-97fe-b51e1b9f6f9e", name = "TestItem15" }, { description = "Description for 16. item", id = ItemId "6d952799-5601-432a-9947-adad57bb54db", name = "TestItem16" }, { description = "Description for 17. item", id = ItemId "365aefff-1fb6-4b3e-84a9-18b1a256b76b", name = "TestItem17" }, { description = "Description for 18. item", id = ItemId "0353f12a-6100-49c4-9c13-262c6efe5019", name = "TestItem18" }, { description = "Description for 19. item", id = ItemId "00a9d79c-8642-4f3c-992c-5feff24da334", name = "TestItem19" }, { description = "Description for 20. item", id = ItemId "76cdfb25-3fd2-4f48-b16e-539968d22bba", name = "TestItem20" } ]
+        , page = { number = 0, size = 20, totalElements = 50, totalPages = 3 }
+        }
+
 
 createBody : String
 createBody =
@@ -86,21 +107,29 @@ createBody =
 
 createExpectedResult : Result Decode.Error ItemsHttpClient.ItemsResponse
 createExpectedResult =
-    Result.Ok { body = { items = [{ description = "Description for first item", id = ItemId "ddc56cf6-1736-4f8d-bfb0-8330cd3d3654", name = "TestItem1" },{ description = "Description for second item", id = ItemId "dacf77b6-5e70-439a-8e3b-d2da974d73ff", name = "TestItem2" },{ description = "Description for 3. item", id = ItemId "21fb97c7-53d4-4395-bc1e-d3e085f396a2", name = "TestItem3" },{ description = "Description for 4. item", id = ItemId "8146f000-ff27-40ed-865c-f58889a0f997", name = "TestItem4" },{ description = "Description for 5. item", id = ItemId "107d3693-0733-409c-9617-f50798cb4c4e", name = "TestItem5" },{ description = "Description for 6. item", id = ItemId "80e85bb1-0647-4445-b586-01900c0d5b34", name = "TestItem6" },{ description = "Description for 7. item", id = ItemId "6406e01b-d5c8-4c40-b551-cf49b3379599", name = "TestItem7" },{ description = "Description for 8. item", id = ItemId "b8c05299-2f0e-4acb-b6d3-27494fb4f404", name = "TestItem8" },{ description = "Description for 9. item", id = ItemId "54f3cc02-a7fd-4ca7-b732-e1f3e27c3984", name = "TestItem9" },{ description = "Description for 10. item", id = ItemId "4b66ed66-4610-4454-af38-a15bdd5c5844", name = "TestItem10" },{ description = "Description for 11. item", id = ItemId "461cfa07-f2c4-4d1e-ada9-5c592b461d17", name = "TestItem11" },{ description = "Description for 12. item", id = ItemId "00fadde0-8b5f-49d3-b6fd-d0f44cf065ed", name = "TestItem12" },{ description = "Description for 13. item", id = ItemId "de718ba8-520c-4199-a819-1674e5582bbe", name = "TestItem13" },{ description = "Description for 14. item", id = ItemId "b1e701fe-7188-47d2-8654-7808ced2f6a7", name = "TestItem14" },{ description = "Description for 15. item", id = ItemId "86aa8336-6bd0-440a-97fe-b51e1b9f6f9e", name = "TestItem15" },{ description = "Description for 16. item", id = ItemId "6d952799-5601-432a-9947-adad57bb54db", name = "TestItem16" },{ description = "Description for 17. item", id = ItemId "365aefff-1fb6-4b3e-84a9-18b1a256b76b", name = "TestItem17" },{ description = "Description for 18. item", id = ItemId "0353f12a-6100-49c4-9c13-262c6efe5019", name = "TestItem18" },{ description = "Description for 19. item", id = ItemId "00a9d79c-8642-4f3c-992c-5feff24da334", name = "TestItem19" },{ description = "Description for 20. item", id = ItemId "76cdfb25-3fd2-4f48-b16e-539968d22bba", name = "TestItem20" }]
-                       , page = { number = 0, size = 20, totalElements = 50, totalPages = 3 }
-                       }
-              , etag = Just "\"dc8b09bc90ed194d61b3111aa371e9dc\""
-              }
+    Result.Ok
+        { body =
+            { items = [ { description = "Description for first item", id = ItemId "ddc56cf6-1736-4f8d-bfb0-8330cd3d3654", name = "TestItem1" }, { description = "Description for second item", id = ItemId "dacf77b6-5e70-439a-8e3b-d2da974d73ff", name = "TestItem2" }, { description = "Description for 3. item", id = ItemId "21fb97c7-53d4-4395-bc1e-d3e085f396a2", name = "TestItem3" }, { description = "Description for 4. item", id = ItemId "8146f000-ff27-40ed-865c-f58889a0f997", name = "TestItem4" }, { description = "Description for 5. item", id = ItemId "107d3693-0733-409c-9617-f50798cb4c4e", name = "TestItem5" }, { description = "Description for 6. item", id = ItemId "80e85bb1-0647-4445-b586-01900c0d5b34", name = "TestItem6" }, { description = "Description for 7. item", id = ItemId "6406e01b-d5c8-4c40-b551-cf49b3379599", name = "TestItem7" }, { description = "Description for 8. item", id = ItemId "b8c05299-2f0e-4acb-b6d3-27494fb4f404", name = "TestItem8" }, { description = "Description for 9. item", id = ItemId "54f3cc02-a7fd-4ca7-b732-e1f3e27c3984", name = "TestItem9" }, { description = "Description for 10. item", id = ItemId "4b66ed66-4610-4454-af38-a15bdd5c5844", name = "TestItem10" }, { description = "Description for 11. item", id = ItemId "461cfa07-f2c4-4d1e-ada9-5c592b461d17", name = "TestItem11" }, { description = "Description for 12. item", id = ItemId "00fadde0-8b5f-49d3-b6fd-d0f44cf065ed", name = "TestItem12" }, { description = "Description for 13. item", id = ItemId "de718ba8-520c-4199-a819-1674e5582bbe", name = "TestItem13" }, { description = "Description for 14. item", id = ItemId "b1e701fe-7188-47d2-8654-7808ced2f6a7", name = "TestItem14" }, { description = "Description for 15. item", id = ItemId "86aa8336-6bd0-440a-97fe-b51e1b9f6f9e", name = "TestItem15" }, { description = "Description for 16. item", id = ItemId "6d952799-5601-432a-9947-adad57bb54db", name = "TestItem16" }, { description = "Description for 17. item", id = ItemId "365aefff-1fb6-4b3e-84a9-18b1a256b76b", name = "TestItem17" }, { description = "Description for 18. item", id = ItemId "0353f12a-6100-49c4-9c13-262c6efe5019", name = "TestItem18" }, { description = "Description for 19. item", id = ItemId "00a9d79c-8642-4f3c-992c-5feff24da334", name = "TestItem19" }, { description = "Description for 20. item", id = ItemId "76cdfb25-3fd2-4f48-b16e-539968d22bba", name = "TestItem20" } ]
+            , page = { number = 0, size = 20, totalElements = 50, totalPages = 3 }
+            }
+        , etag = Just "\"dc8b09bc90ed194d61b3111aa371e9dc\""
+        }
 
-itemsResponseFromResult : (Result Decode.Error ItemsHttpClient.ItemsResponse) -> ItemsHttpClient.ItemsResponse
+
+itemsResponseFromResult : Result Decode.Error ItemsHttpClient.ItemsResponse -> ItemsHttpClient.ItemsResponse
 itemsResponseFromResult result =
     case result of
-        Result.Ok value -> value
-        _ -> { body = { items = []
-                      , page = { number = 0, size = 20, totalElements = 50, totalPages = 3 }
-                      }
-             , etag = Nothing
-             }
+        Result.Ok value ->
+            value
+
+        _ ->
+            { body =
+                { items = []
+                , page = { number = 0, size = 20, totalElements = 50, totalPages = 3 }
+                }
+            , etag = Nothing
+            }
+
 
 createExpectedMsg : ListItems.Msg
 createExpectedMsg =
